82fb67a05a95df6febe25ee121d76cb2f38f7d59
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2021-11-06 01:53:07 -0300

    organizando

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,11 +1,6 @@
 import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repositories.scss";
 import { useEffect, useState } from "react";
-const repository = {
-  name: "unform",
-  description: "Forms in ReactJS",
-  link: "https://github.com/unform/unform",
-};
 export function RepositoryList() {
   // quando comeca um state com uma lista vazia, entao passa um array vazio, cochetes vazios
   const [repositories, setRepositores] = useState([]);
@@ -17,15 +12,13 @@ export function RepositoryList() {
       .then((response) => response.json())
       .then((data) => setRepositores(data));
   }, []);
-  console.log(repositories)
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => {
+          return <RepositoryItem repository={repository} />;
+        })}
       </ul>
     </section>
   );
