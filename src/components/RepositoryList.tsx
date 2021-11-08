207f1b82b5ab5809b2d9918006ce8fcc7a478346
Filipe Sales <66375370+filipesalesaraujo@users.noreply.github.com>
2021-11-07 22:30:52 -0300

    Componentes com TypeScript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,9 +1,14 @@
 import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repositories.scss";
 import { useEffect, useState } from "react";
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
 export function RepositoryList() {
   // quando comeca um state com uma lista vazia, entao passa um array vazio, cochetes vazios
-  const [repositories, setRepositores] = useState([]);
+  const [repositories, setRepositores] = useState<Repository[]>([]);
   // useEffect dispara uma função quando algo acontece na aplicação. O algo pode ser uma variavel mudar. Se ela mudar, posso querer avisar uma API que ela mudou, ou disparar uma função.
   // useEffect recebe dois parametros, o primeiro deles é qual função eu quero executar () => {}.
   // O segundo parametro, é quando quero quando a primeira funcao ocorra []. Passamos um array de depedencias, useEffect rodar a funcao
@@ -17,7 +22,9 @@ export function RepositoryList() {
       <h1>Lista de repositórios</h1>
       <ul>
         {repositories.map((repository) => {
-          return <RepositoryItem key={repository.name} repository={repository} />;
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
         })}
       </ul>
     </section>
